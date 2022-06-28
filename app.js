1cef364b9ed9f8f0ca6e176027ed268b7e66b751
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-06-28 14:06:18 -0300

    Peticiones, respuestas y templates

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+// -------FORMULARIO
+require('dotenv').config();
+
 // -------CONTROLADORES 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET del Index */
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
 module.exports = router;
+
+/* POST del Index */
+var nodemailer = require('nodemailer');
+
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var mail = req.body.mail;
+  var mensaje = req.body.mensaje;
+  console.log(req.body);
+  var destinatario = {
+    to: 'sergiosainz.proyectos@gmail.com'
+    subject: 'Mensaje de la web Coach - Cristian Garmendia'
+    html: nombre + '' + 'Necesita más información sobre los talleres. Responder a: ' + mail + 'Agregó el siguiente mensaje' + mensaje + '.'
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    }
+  });
+
+  var info = await transport.sendMail(destinatario);
+
+  res.render('index', {
+    mensaje: 'Usted, envió su mensaje correctamente. Le contestaremos a la brevedad'
+  });
+
+});
